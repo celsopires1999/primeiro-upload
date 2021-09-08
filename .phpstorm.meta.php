d0c3892ada083cc10fd89e6bb1d49df63e6b3863
celsopires1999 <celso.pires1999@gmail.com>
2021-09-08 20:48:12 -0300

    add Test Reviewed

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2021-09-08 20:03:23.
+    * Generated on 2021-09-08 23:31:41.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
