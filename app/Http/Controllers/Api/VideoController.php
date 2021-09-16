65b690ce5d8a0a30062ead04d512397777e9c1d3
celsopires1999 <celso.pires1999@gmail.com>
2021-09-15 23:16:47 -0300

    add criando resource de video

---
+++ app/Http/Controllers/Api/VideoController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Video;
+use Illuminate\Http\Request;
+
+class VideoController extends BasicCrudController
+{
+    private $rules = [
+    ];
+
+    public function __construct()
+    {
+        
+    }
+
+    protected function model()
+    {
+        return Video::class;
+    }
+
+    protected function rulesStore()
+    {
+        return $this->rules;
+    }
+
+    protected function rulesUpdate()
+    {
+        return $this->rules;
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
         $this->call(GenresTableSeeder::class);
-        $this->call(CastMemberTableSeeder::class);
+        $this->call(CastMembersTableSeeder::class);
+        $this->call(VideosTableSeeder::class);
     }
 }
